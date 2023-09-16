8cb2209cde99d265b1b0af194fd44cd6de0f2597
unknown <zaynzafar18@gmail.com>
2023-09-16 17:06:52 +0500

    make some changes in LandingHero.tsx

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -12,9 +12,9 @@ export const LandingHero = () => {
 
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
-      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold tracking-tighter bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-300 ">
         <h1>The Best AI Tool for</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#36b49f]/90 to-[#DBFF75]/90 pb-2">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-[#36b49f]/90 to-[#DBFF75]/90 pb-2 tracking-tighter">
           <TypewriterComponent
             options={{
               strings: [
@@ -29,14 +29,14 @@ export const LandingHero = () => {
           />
         </div>
       </div>
-      <div className="text-sm md:text-xl font-light text-zinc-400">
+      <div className="text-sm md:text-xl font-light text-zinc-300 tracking-tighter">
         Create content using AI 10x faster.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button
             variant="premium"
-            className="md:text-lg p-4 md:p-6 rounded-full font-semibold"
+            className="md:text-lg p-4 md:p-6 rounded-full font-semibold tracking-tight"
           >
             Start Generating For Free
           </Button>
